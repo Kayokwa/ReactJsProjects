74fa835241456674b4d81dc15b6f8993bf120cd5
Kayokwa Chibuye <kayokwa.chibuye@gmail.com>
2021-03-14 22:25:35 -0700

    Created subcomponents

--- reactingonlineappointments/src/components/App.js
+++ reactingonlineappointments/src/components/App.js
@@ -1,21 +1,29 @@
+import { render } from '@testing-library/react';
+import React, {Component} from 'react';
 import '../css/App.css';
 
-function App() {
+import AddAppointments from './AddAppointments';
+import ListAppointments from './ListAppointments';
+import SearchAppointments from './SearchAppointments';
+
+class App extends Component {
+  render(){
   return (
     <main className="page bg-white" id="petratings">
     <div className="container">
       <div className="row">
         <div className="col-md-12 bg-white">
           <div className="container">
-            <div>Add Appointments</div>
-            <div>Search Appointments</div>
-            <div>List Appointments</div>
+            <AddAppointments/>
+            <SearchAppointments/>
+            <ListAppointments/>
           </div>
         </div>
       </div>
     </div>
   </main>
   );
+}
 }
 
 export default App;
